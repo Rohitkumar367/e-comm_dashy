b39f625d6d392d56cc94ea5427a98ada8182ba58
Rohitkumar367 <rohitddroid@gmail.com>
2024-10-21 00:13:42 +0530

    prepared for login page

--- 01_backendSETUP/index.js
+++ 01_backendSETUP/index.js
@@ -21,13 +21,41 @@ app.get('/', (req, resp) => {
     resp.send("hello mr_d_droid")
 })
 
-app.post('/register', async (req, res)=>{
-    let user = new User(req.body);
-    let result = await user.save();
+app.post('/register', async (req, res) => {
+    let user = new User(req.body);// to send data to database we first formed the object of that data based on User model
+    let result = await user.save();// .save() is used to send the data, we can't use .select with .save so we manually delete the password in result variable
+    result = result.toObject();
+    delete result.password;
+
     res.send(result)
 })
 
-app.listen(5000, ()=>{
+app.post('/login', async (req, resp) => {
+
+    console.log(req.body)
+
+    // to check user has sent both password and email
+    if (req.body.password && req.body.email) 
+    {
+        let user = await User.findOne(req.body).select("-password"); // findOne is used to find the data in database and .select("-password") used to fetch data without password
+
+        if(user) 
+        {
+            resp.send(user);
+        }
+        else 
+        {
+            resp.send({ result: 'no user found' })
+        }
+    }
+    else
+    {
+        resp.send({result: 'no user found'})
+    }
+
+})
+
+app.listen(5000, () => {
     console.log('Backend Server is running at:-- http://localhost:5000')
 })
 
